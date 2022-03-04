f9d8e9cfe282ffee647dca44436cac53984d38e0
loubnaAminou <loubnaaminou0@gmail.com>
2022-03-04 16:06:22 +0100

    Ioc : loose coupling

---
+++ InversionControl/src/main/java/presentation/PresInstanDynamic.java
@@ -0,0 +1,27 @@
+package presentation;
+
+import dao.IDao;
+import metier.IMetier;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+public class PresInstanDynamic {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(new File("config.txt"));
+
+        String daoClassName = scanner.nextLine();
+        Class cDao = Class.forName(daoClassName); // Charger la classe en mémoire
+        IDao dao = (IDao)cDao.newInstance();
+        System.out.println(dao.getData());
+
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cMetier.newInstance();
+
+        Method method = cMetier.getMethod("setDao", IDao.class);
+        method.invoke(metier, dao);
+        System.out.println(metier.calcul());
+    }
+}
