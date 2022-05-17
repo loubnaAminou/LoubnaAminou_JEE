9093ce16b4ba5be504680c6706185915c19251b9
loubnaAminou <loubnaaminou0@gmail.com>
2022-05-17 11:54:22 +0100

    New Customer Form

---
+++ Digital Banking/FrontEnd/src/app/new-customer/new-customer.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Customer} from "../model/customer";
+import {CustomerService} from "../services/customer.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-new-customer',
+  templateUrl: './new-customer.component.html',
+  styleUrls: ['./new-customer.component.css']
+})
+export class NewCustomerComponent implements OnInit {
+
+  newCustomerForm! : FormGroup;
+  constructor(private formBuilder : FormBuilder, private customerService : CustomerService, private router : Router) { }
+
+  ngOnInit(): void {
+    this.newCustomerForm = this.formBuilder.group({
+      name : this.formBuilder.control("Jane Doe", [Validators.required, Validators.minLength(4)]),
+      email : this.formBuilder.control("Jane@Doe", [Validators.required, Validators.email])
+    });
+  }
+
+  handleNewCustomer() {
+    let customer : Customer = this.newCustomerForm.value
+      this.customerService.newCustomer(customer).subscribe({
+        next : value => this.router.navigateByUrl("/customers"),
+        error : err => console.log(err)
+      });
+  }
+}
